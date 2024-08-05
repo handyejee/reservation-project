552a7fb7cb5f7d5b7622f78d2865a94e671ff5a4
yjson <handyejee14@gmail.com>
2024-08-05 16:04:19 +0900

    [fix/1] Member, Partner 별 권한 명확히 하기 위해 enum 명 admin으로 수정, 패키지 명 변경

--- src/main/java/livoi/reservation/model/constants/Authority.java
+++
@@ -1,7 +0,0 @@
-package livoi.reservation.model.constants;
-
-public enum Authority {
-
-    ROLE_READ,
-    ROLE_WRITE;
-}
---
+++ src/main/java/livoi/reservation/type/Authority.java
@@ -0,0 +1,7 @@
+package livoi.reservation.type;
+
+public enum Authority {
+
+    ROLE_READ,
+    ROLE_ADMIN;
+}
